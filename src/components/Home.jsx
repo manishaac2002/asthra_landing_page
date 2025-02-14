2dd8b516696b936da8833b4ef3f2ea125a586719
manishac2002 <manishaac2002@gmail.com>
2025-02-14 11:59:48 +0530

    completed responsive for all pages.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,13 @@
-import armRobo from "../images/ArmRobo.webp"
+import armRobo from "../images/Robots.webp"
 
 
 const Home = () => {
   return (
     <>
-      <div id="home" className="absolute left-4 md:left-30 lg:left-50 xl:left-100 2xl:left-200 pb-52 w-sm md:w-xl lg:w-2xl xl:w-3xl 2xl:w-4xl">
+      <div id="home" className="absolute left-4 md:left-24 lg:left-80 xl:left-60 2xl:left-80 opacity-100 pt-24 w-sm md:w-xl lg:w-2xl xl:w-3xl 2xl:w-7xl">
         <img src={armRobo} alt="armrobo" />
       </div>
-    <div className=" pl-10 sm:pl-24 md:pl-10 lg:pl-38 xl:pl-68 mt-38 md:mt-56 lg:mt-68 xl:mt-72 mr-10 lg:mr-20 xl:mr-40 ">
+    <div className="relative pl-10 sm:pl-24 md:pl-10 lg:pl-38 xl:pl-68 mt-38 md:mt-56 lg:mt-68 xl:mt-72 mr-10 lg:mr-20 xl:mr-40 ">
         <p className=" text-xl sm:text-3xl xl:text-4xl 2xl:text-7xl font-bold text-[#213B6D] text-justify">Transforming care with innovative and precision 
         driven <span className="text-[#FFA726]">Robotic solutions.</span> </p>
     </div>
