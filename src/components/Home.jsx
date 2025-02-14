5fa7171d5378b3ebacaf2cf4e58a8bc29a884dfd
manishac2002 <manishaac2002@gmail.com>
2025-02-14 12:01:54 +0530

    completed responsive for all pages.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import armRobo from "../images/Robots.webp"
 const Home = () => {
   return (
     <>
-      <div id="home" className="absolute left-4 md:left-24 lg:left-80 xl:left-60 2xl:left-80 opacity-100 pt-24 w-sm md:w-xl lg:w-2xl xl:w-3xl 2xl:w-7xl">
+      <div id="home" className="absolute left-4 md:left-24 lg:left-50 xl:left-70 2xl:left-80 opacity-100 pt-24 w-sm md:w-xl lg:w-2xl xl:w-3xl 2xl:w-7xl">
         <img src={armRobo} alt="armrobo" />
       </div>
     <div className="relative pl-10 sm:pl-24 md:pl-10 lg:pl-38 xl:pl-68 mt-38 md:mt-56 lg:mt-68 xl:mt-72 mr-10 lg:mr-20 xl:mr-40 ">
