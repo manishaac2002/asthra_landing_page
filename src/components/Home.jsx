6310ee40f72e0325fb5441f05b4c195fb42a4fe5
manishac2002 <manishaac2002@gmail.com>
2025-02-19 16:48:56 +0530

    style : changed the font.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,13 @@
-import armRobo from "../images/Robots.webp"
+
 
 
 const Home = () => {
   return (
     <>
-      <div id="home" className="absolute left-4 md:left-24 lg:left-50 xl:left-70 2xl:left-80 opacity-100 pt-24 w-sm md:w-xl lg:w-2xl xl:w-3xl 2xl:w-7xl">
-        <img src={armRobo} alt="armrobo" />
-      </div>
-    <div className="relative pl-10 sm:pl-24 md:pl-10 lg:pl-38 xl:pl-68 mt-38 md:mt-56 lg:mt-68 xl:mt-72 mr-10 lg:mr-20 xl:mr-40 ">
-        <p className=" text-xl sm:text-3xl xl:text-4xl 2xl:text-7xl font-bold text-[#213B6D] text-justify">Transforming care with innovative and precision 
-        driven <span className="text-[#FFA726]">Robotic solutions.</span> </p>
+      
+    <div id="home" className=" text-center pl-10 sm:pl-24 md:pl-10 lg:pl-38 xl:pl-68 mt-38 md:mt-56 lg:mt-68 xl:mt-72 mr-10 lg:mr-20 xl:mr-40 ">
+        <p className="roboto text-[5rem] font-bold text-[#213B6D]">Transforming care with innovative and precision 
+        <br />driven <span className="text-[#FFA726] " >Robotic solutions.</span> </p>
     </div>
     </>
   );
