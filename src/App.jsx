0fde9967fa248910c8041c64b560c991283a20b7
manishac2002 <manishaac2002@gmail.com>
2025-03-04 11:47:33 +0530

    feat - changed the pages to navigate.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-
-import './App.css'
-import Home from './components/Home'
-import NavBar from './components/NavBar'
-import AboutUs from './components/AboutUs'
-import ProductServices from './components/ProductServices'
-import Contact from './components/Contact'
-import Career from './components/Career'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
+import AboutUs from './components/AboutUs';
+import ProductServices from './components/ProductServices';
+import Contact from './components/Contact';
+import Career from './components/Career';
 
 function App() {
-
   return (
-    <>
-   <NavBar/>
-   <Home />
-   <AboutUs />
-   <ProductServices />
-   <Career/>
-   <Contact/>
-   </>
-  )
+    <Router>
+      <NavBar />  {/* Keep NavBar outside Routes to make it persistent */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/products" element={<ProductServices />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,33 @@
+import { Link } from "react-router-dom";
 import { AsthraLogo } from "../Icons/AsthraPattern";
 
 const NavBar = () => {
   return (
-    <div className="flex sticky top-0 bg-white shadow-md z-50 p-4 ">
-      <div className="">
-        <a href="#home">
+    <div className="flex sticky top-0 bg-white shadow-md z-50 p-4">
+      {/* Logo */}
+      <div>
+        <Link to="/">
           <AsthraLogo />
-        </a>
+        </Link>
       </div>
 
+      {/* Navigation Links */}
       <div className="md:ml-auto hidden md:block">
         <ul className="flex">
           <li className="px-2 lg:px-4 xl:px-6 text-sm lg:text-lg text-[#213B6D] hover:text-[#FFA726] hover:font-semibold transition-all duration-300 ease-in-out">
-            <a href="#home">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li className="px-2 lg:px-4 xl:px-6 text-sm lg:text-lg text-[#213B6D] hover:text-[#FFA726] hover:font-semibold transition-all duration-300 ease-in-out">
-            <a href="#about">About Us</a>
+            <Link to="/about">About Us</Link>
           </li>
           <li className="px-2 lg:px-4 xl:px-6 text-sm lg:text-lg text-[#213B6D] hover:text-[#FFA726] hover:font-semibold transition-all duration-300 ease-in-out">
-            <a href="#products">Product & Services</a>
+            <Link to="/products">Product & Services</Link>
           </li>
           <li className="px-2 lg:px-4 xl:px-6 text-sm lg:text-lg text-[#213B6D] hover:text-[#FFA726] hover:font-semibold transition-all duration-300 ease-in-out">
-            <a href="#careers">Careers</a>
+            <Link to="/career">Careers</Link>
           </li>
           <li className="px-2 lg:px-4 xl:px-6 text-sm lg:text-lg text-[#213B6D] hover:text-[#FFA726] hover:font-semibold transition-all duration-300 ease-in-out">
-            <a href="#contact">Contact</a>
+            <Link to="/contact">Contact</Link>
           </li>
         </ul>
       </div>
